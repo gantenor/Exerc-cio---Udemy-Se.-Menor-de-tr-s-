d628630f8231236848364120c2cd1c79b8a56470
Guilherme Santana <>
2021-04-29 19:54:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var a, b, c, menor
+
+a = parseInt(a);
+a = prompt("Primeiro valor: ");
+
+b = parseInt(b);
+b = prompt("Segundo valor: ");
+
+c = parseInt(c);
+c = prompt("Terceiro valor: ");
+
+if (a>b && a>c){
+  alert('Menor = ' + a);
+}
+  else if (b<a && b<c){
+    alert('Menor =' + b);
+  }  
+      else{
+        (c>a && c>b) 
+        alert('Menor = ' + c);
+      } 
+  
+     
+     
